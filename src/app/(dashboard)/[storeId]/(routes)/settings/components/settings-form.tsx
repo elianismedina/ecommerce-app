fb55478aac8402ec6b36950812727fff5c4eccc9
elianisdev <elianismedina05@outlook.com>
2023-09-20 12:46:06 -0500

    added heading componenet

--- src/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { Store } from "@prisma/client";
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+
 
 interface SettingsFormProps {
     initialData: Store;
@@ -12,9 +16,20 @@ export  const SettingsForm : React.FC<SettingsFormProps> = ({
     initialData
 }) => {
     return(
-        <div>
-            Settings Form
+        <div className="flex items-center justify-between">
 
+            <Heading
+            title="Settings"
+            description= "Manage your store settings"
+            />
+            <Button
+            variant="destructive"
+            size="icon"
+            onClick={() => {}}
+            >
+                <Trash className="h-4 w-4"/>
+
+            </Button>
         </div>
     );
 };
