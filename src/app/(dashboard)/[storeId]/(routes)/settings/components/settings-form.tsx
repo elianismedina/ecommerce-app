7d98c5e5371944f1ddcccd6ca99dc4a8b3088ace
elianisdev <elianismedina05@outlook.com>
2023-09-20 11:58:43 -0500

    added seetings props

---
+++ src/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Store } from "@prisma/client";
+
+interface SettingsFormProps {
+    initialData: Store;
+    
+
+}
+
+export  const SettingsForm : React.FC<SettingsFormProps> = ({
+    initialData
+}) => {
+    return(
+        <div>
+            Settings Form
+
+        </div>
+    );
+};
