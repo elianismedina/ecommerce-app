241af15d75a9492fce63102c186772ae3906f1d9
elianisdev <elianismedina05@outlook.com>
2023-09-22 19:55:31 -0500

    added delete function

--- src/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -18,6 +18,10 @@ import { Form,
          FormMessage
          } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
+import { AlertModal } from "@/components/modals/alert-modal";
 
 
 interface SettingsFormProps {
@@ -33,6 +37,10 @@ export  const SettingsForm : React.FC<SettingsFormProps> = ({
     initialData
 }) => {
 
+    const params = useParams();
+    const router = useRouter();
+
+
     const [open, setOpen] = useState(false);
     const [loading, setLoading] = useState(false);
 
@@ -42,11 +50,43 @@ export  const SettingsForm : React.FC<SettingsFormProps> = ({
     });
 
     const onSubmit = async (data: SettingsFormValues) => {
-        console.log(data);
+        try{
+            setLoading(true);
+            await axios.patch(`/api/stores/${params.storeId}`, data);
+            router.refresh();
+            toast.success("Store updated!");
+
+        } catch (error) {
+            toast.error("Something went wrong!");
+        } finally{
+            setLoading(false);
+        }
     };
+
+    const onDelete = async () => {
+        try{
+            setLoading(true);
+            await axios.delete(`/api/stores/${params.storeId}`);
+            router.refresh();
+            router.push("/");
+            toast.success("Store deleted!");
+
+        } catch (error) {
+            toast.error("Make sure you deleted all products and categories first!");
+        } finally{
+            setLoading(false)
+            setOpen(false)
+        }
+    }
 
     return (
       <>
+        <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+        />
         <div className="flex items-center justify-between">
           <Heading title="Settings" description="Manage your store settings" />
           <Button disabled={loading} variant="destructive" size="icon" onClick={() => setOpen(true)}>
