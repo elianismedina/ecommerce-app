cad1e085dac592f3a52861c429e7fcad9affde6c
elianisdev <elianismedina05@outlook.com>
2023-09-24 17:04:05 -0500

    added create billboard

--- src/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,8 +1,31 @@
-const BillboardPage = () => {
-    return ( 
-        <div>
-            This is a form for billboards
+import prismadb from "@/lib/prismadb";
+import { BillboardForm } from "./components/billboard-form";
+
+const BillboardPage = async ({
+    params
+}:{
+    params:{ billboardId: string }
+}) => {
+
+    const billboard = await prismadb.billboard.findUnique({
+        where: {
+            id: params.billboardId
+        }
+    });
+
+
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <BillboardForm
+                initialData={billboard}
+                />
+
+            </div>
+
         </div>
+        
      );
 }
  
