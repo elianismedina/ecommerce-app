f6cf99bd98b4a2a3f3f7de1f424f5cf8796773a0
elianisdev <elianismedina05@outlook.com>
2023-10-05 14:11:58 -0500

    added stripe

--- src/app/api/[storeId]/categories/[categoryId]/route.ts
+++ src/app/api/[storeId]/categories/[categoryId]/route.ts
@@ -14,6 +14,10 @@ export async function GET(
         const category = await prismadb.category.findUnique({
             where: {
                 id: params.categoryId,
+            },
+            include: {
+                billboard: true,
+                
             }
         });
 
